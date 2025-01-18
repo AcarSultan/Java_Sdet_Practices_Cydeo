576186c7d44e24a0041f48fcd132153548410171
AcarSultan <sultanacar11@gmail.com>
2025-01-18 15:33:31 +0300

    place an order task solution

---
+++ src/place_an_order/PlaceAnOrder.java
@@ -0,0 +1,45 @@
+package place_an_order;
+
+import java.util.Scanner;
+
+public class PlaceAnOrder {
+    // Ask the user to enter the product name (multiple words).
+    //    Ask the user to enter the price (double).
+    //    Ask the user to enter the quantity (int).
+    //    Ask the user to enter their first name (String, single word).
+    //
+    //    Print in the following format:
+    //
+    //        Example:
+    //             Inputs: "Apples", 1.5, 5, "Luke"
+    //
+    //        Output:
+    //             Luke, your order for 5 Apples has been placed. Your total is 7.5.
+
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the product name: ");
+        String productName= input.nextLine();
+
+        System.out.println("Enter the price: ");
+        double price = input.nextDouble();
+
+        System.out.println("Enter the quantity: ");
+        int quantity = input.nextInt();
+
+        System.out.println("Enter your first name: ");
+        String firstName= input.next();
+
+
+        System.out.println(firstName + ", your order for " + quantity + " " + productName + " has been placed. Your total is " + price + ".");
+
+
+
+
+
+
+    }
+}
