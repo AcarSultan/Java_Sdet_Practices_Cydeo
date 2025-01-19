da4945e6e040b58862776bb52971a661b03c2828
AcarSultan <sultanacar11@gmail.com>
2025-01-19 09:24:24 +0300

    branching statement return

---
+++ src/branching_statement_return/BranchingStatementReturn.java
@@ -0,0 +1,18 @@
+package branching_statement_return;
+
+public class BranchingStatementReturn {
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 6; i++) {
+            if (i==3){
+                // continue; // skips // jumps to the next iteration of the loop
+                // break; // terminates or exits the loop // jumps out of the loop's body
+                // return; // terminates the method pr exits the method // jumps out of the method
+            }
+            System.out.println(i);
+        }
+
+        System.out.println("Hello Cydeo");
+
+    }
+}
