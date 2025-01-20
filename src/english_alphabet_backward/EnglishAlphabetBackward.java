570d9fd6c5bbd127f814782f1c81c663ec4d643f
AcarSultan <sultanacar11@gmail.com>
2025-01-20 10:08:03 +0300

    English alphabet from backwards

---
+++ src/english_alphabet_backward/EnglishAlphabetBackward.java
@@ -0,0 +1,12 @@
+package english_alphabet_backward;
+
+public class EnglishAlphabetBackward {
+    public static void main(String[] args) {
+
+        for (char i = 'Z'; i >= 'A'; i--) {
+            System.out.print(i + " ");
+
+        }
+
+    }
+}
