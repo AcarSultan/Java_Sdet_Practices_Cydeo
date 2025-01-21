fce5b38b396d496011fa52533219cc5f6da8e50a
AcarSultan <sultanacar11@gmail.com>
2025-01-21 16:09:07 +0300

    PizzaClient class

---
+++ src/pizza/PizzaClient.java
@@ -0,0 +1,12 @@
+package pizza;
+
+public class PizzaClient {
+
+    public static void main(String[] args) {
+
+        Pizza pizza= new Pizza("small", 2, 2);
+
+        System.out.println(pizza);
+    }
+
+}
