0ab4a51afc30d6e157023037cf458fdb90f8331c
AcarSultan <sultanacar11@gmail.com>
2025-01-27 17:05:44 +0300

    diamond shape with java

---
+++ src/diamond/Diamond.java
@@ -0,0 +1,18 @@
+package diamond;
+
+public class Diamond {
+
+    public static void main(String[] args) {
+        System.out.println("     ^");
+        System.out.println("   /   \\");
+        System.out.println("  /     \\");
+        System.out.println(" /       \\");
+        System.out.println(" ---------");
+        System.out.println(" \\       /");
+        System.out.println("  \\     /");
+        System.out.println("   \\   /");
+        System.out.println("     v");
+    }
+
+
+}
