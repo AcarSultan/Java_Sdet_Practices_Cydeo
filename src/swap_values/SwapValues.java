1860d01765c4330c593d91bf5073b2bc0ffd9912
AcarSultan <sultanacar11@gmail.com>
2025-02-03 00:11:35 +0300

    swap values task solution

--- src/swap_values/SwapValues.java
+++ src/swap_values/SwapValues.java
@@ -2,35 +2,55 @@ package swap_values;
 
 public class SwapValues {
 
+     /*
+    Swap Numbers:
+    Swap two integer variable values (including both ways; with using a third variable and without it)
+     */
+
+
     public static void main(String[] args) {
 
+        // First method
+        swappingNumbers(8,9);
+
+        // Second method
+        System.out.println(swappingNumbers2(1905, 2025));
+    }
 
 
-        int num1 = 5;
-        int num2 = 10;
+
+
+    /**
+     * swapping numbers with a third variable called 'temp' and used the return type 'void'.
+     * @param num1
+     * @param num2
+     */
+    public static void swappingNumbers(int num1, int num2){
+
         int temp = 0;
-
 
         temp = num1;
         num1=num2;
         num2=temp;
 
-        System.out.println(num1);
-        System.out.println(num2);
-
-        System.out.println("-------------------------------------------");
-
-        int num3 = 5;
-        int num4 = 10;
-
-        num3 = num3 + num4;
-        num4 = num3 - num4;
-        num3 = num3 - num4;
-
-        System.out.println(num3);
-        System.out.println(num4);
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+    }
 
 
 
+    /**
+     * swapping numbers without any third variable and used a String return method.
+     * @param num1
+     * @param num2
+     * @return
+     */
+    public static String swappingNumbers2(int num1, int num2){
+
+        num1 = num1 + num2;
+        num2 = num1 - num2;
+        num1 = num1 - num2;
+
+        return ("num1= " + num1 + "\nnum2= " + num2);
     }
 }
