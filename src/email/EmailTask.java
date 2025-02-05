51138584c0f5a87063361c40e39c50aeac5d8fa7
AcarSultan <sultanacar11@gmail.com>
2025-02-05 16:24:50 +0300

    Calculating factorial numbers with a custom int method

--- src/email/EmailTask.java
+++ src/email/EmailTask.java
@@ -34,7 +34,9 @@ public class EmailTask {
             System.out.println(email);
         }
 
-
-
     }
+
+
+
+
 }
--- src/factorial_number/FactorialNumbers.java
+++ src/factorial_number/FactorialNumbers.java
@@ -3,15 +3,31 @@ package factorial_number;
 public class FactorialNumbers {
     public static void main(String[] args) {
 
-        // 5!= 5*4*3*2*1 ---> 120
-        int num= 5;
-        int factorialNumber= 1;
 
-        for (int i = 1; i <=num ; i++) {
-            factorialNumber= i * factorialNumber;
-        }
-
-        System.out.print(factorialNumber);
+        System.out.println(calculatingFactorial(0));
+        System.out.println(calculatingFactorial(5));
+        System.out.println(calculatingFactorial(8));
+        System.out.println(calculatingFactorial(12));
 
     }
+
+    /**
+     * Custom int method for calculating factorial numbers
+     * @param number
+     * @return factorialNumber
+     */
+    public static int calculatingFactorial(int number) {
+
+        // An example how factorial numbers work : 5!= 5*4*3*2*1 ---> 120
+
+        int factorialNumber = 1;
+        for (int i = 1; i <= number; i++) {
+            factorialNumber = i * factorialNumber;
+        }
+        return factorialNumber;
+    }
+
+
+
+
 }
