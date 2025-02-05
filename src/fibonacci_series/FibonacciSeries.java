d686e9dff363536068a41d573f757f85a25c1216
AcarSultan <sultanacar11@gmail.com>
2025-02-05 16:34:24 +0300

    Fibonacci series calculating task solution

--- src/fibonacci_series/FibonacciSeries.java
+++ src/fibonacci_series/FibonacciSeries.java
@@ -4,24 +4,43 @@ public class FibonacciSeries {
 
     public static void main(String[] args) {
 
-        // input : 8
-        // output :  0 1 1 2 3 5 8 13
 
+        fibonacciCalculating(8);
+
+
+    }
+
+
+    /**
+     * A custom method for Fibonacci series calculating
+     * @param number
+     * @return sum
+     */
+    public static int fibonacciCalculating(int number){
+
+        //The Fibonacci sequence is a series of numbers where each number
+        //is the sum of the two preceding ones, usually starting with 0 and 1.
+        // input : 8
+        // output :  0, 1, 1, 2, 3, 5, 8, 13, and so on.
 
         int num1 = 0;
         int num2 = 1;
         int sum = 0;
 
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < number; i++) {
 
             System.out.print(num1 + " ");
 
             sum = num1 + num2;
             num1 = num2;
             num2 = sum;
-
-
         }
 
+        return sum;
+
     }
+
+
+
+
 }
