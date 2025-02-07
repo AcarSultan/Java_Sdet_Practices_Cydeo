dda0c3f12c5f661992f1e4140b4817a2dd1fda14
AcarSultan <sultanacar11@gmail.com>
2025-02-07 19:51:02 +0300

    a custom arrayList method for moving zeros to the end of the list

--- src/move_the_zeros/MoveTheZeros.java
+++ src/move_the_zeros/MoveTheZeros.java
@@ -4,24 +4,34 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
-public class MoveTheZeros{
+public class MoveTheZeros {
     public static void main(String[] args) {
 
-        ArrayList<Integer> list= new ArrayList<>(Arrays.asList(1,0,2,0,3,0,4,0));
-        ArrayList<Integer> newList= new ArrayList<>();
+        ArrayList result = movingTheZeros(new ArrayList<>(Arrays.asList(1, 0, 2, 0, 3, 0, 4, 0)));
+        System.out.println(result);
 
-        int numOfZeroes= Collections.frequency(list,0);
+    }
+
+    /**
+     * a custom arrayList method for moving zeros to the end of the list
+     * @param list ArrayList<Integer>
+     * @return list
+     */
+    public static ArrayList movingTheZeros(ArrayList<Integer> list) {
+
+        //list= new ArrayList<>(Arrays.asList(1,0,2,0,3,0,4,0));
+        ArrayList<Integer> newList = new ArrayList<>();
+
+        int numOfZeroes = Collections.frequency(list, 0);
         list.removeAll(Arrays.asList(0));
 
-
-        for (int i=0; i<numOfZeroes; i++){
+        for (int i = 0; i < numOfZeroes; i++) {
             list.add(0);
         }
 
-        System.out.println(list);
-
-
-
+        return list;
     }
+
+
 }
 
