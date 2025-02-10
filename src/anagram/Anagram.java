69b077eabcf46eb43d4b58a19aa82c95393ffcf4
AcarSultan <sultanacar11@gmail.com>
2025-02-10 16:57:01 +0300

    finding anagram strings

--- src/anagram/Anagram.java
+++ src/anagram/Anagram.java
@@ -8,19 +8,36 @@ public class Anagram {
 
     public static void main(String[] args) {
 
-        String str1 = "abcd";
-        String str2 = "ccddabaa";
+        System.out.println(findAnagram("abcd", "ccddabaa"));
+        System.out.println(findAnagram("ananas", "monopoly"));
+        System.out.println(findAnagram("dusty", "stu dy"));
+
+    }
+
+    /**
+     * A custom method for finding the anagrams
+     * @param str1
+     * @param str2
+     * @return set1.equals(set2)
+     */
+    public static boolean findAnagram(String str1, String str2){
+
+        // Remove spaces and convert to lowercase for case-insensitive comparison
+        str1 = str1.replaceAll("\\s+", "").toLowerCase();
+        str2 = str2.replaceAll("\\s+", "").toLowerCase();
 
         Set<String> set1 = new TreeSet<>(Arrays.asList(str1.split("")));
         Set<String> set2 = new TreeSet<>(Arrays.asList(str2.split("")));
 
-        if (set1.equals(set2)){
-            System.out.println("True");
-        } else {
-            System.out.println("It's not anagram!");
-        }
+        return set1.equals(set2);
 
 
     }
+
+
+
+
+
+
 
 }
