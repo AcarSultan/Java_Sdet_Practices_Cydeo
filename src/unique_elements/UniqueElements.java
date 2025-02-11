9e7fab148a86d81ffa997b57064f829a0c3e694e
AcarSultan <sultanacar11@gmail.com>
2025-02-11 16:19:50 +0300

    finding the unique elements

--- src/unique_elements/UniqueElements.java
+++ src/unique_elements/UniqueElements.java
@@ -8,14 +8,24 @@ public class UniqueElements {
 
     public static void main(String[] args) {
 
-        ArrayList<Integer> list= new ArrayList<>(Arrays.asList(1, 1, 2, 3, 3, 4, 5, 5));
-
-        list.removeIf (p -> Collections.frequency(list,p) > 1);
-        System.out.println(list);
-
-
-
-
+        ArrayList<Integer> result = findUniqueElement(new ArrayList<>(Arrays.asList(1, 1, 2, 3, 3, 4, 5, 5)));
+        System.out.println(result);
 
     }
+
+
+    /**
+     * A custom ArrayList method for finding the unique elements
+     * @param list
+     * @return list
+     */
+    public static ArrayList findUniqueElement(ArrayList<Integer> list){
+
+        list.removeIf (p -> Collections.frequency(list,p) > 1);
+        return list;
+
+    }
+
+
+
 }
