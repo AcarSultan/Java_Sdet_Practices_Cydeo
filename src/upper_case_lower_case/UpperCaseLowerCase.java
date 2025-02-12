f1eddf956b24ccdd48ba3e695de958636b40917e
AcarSultan <sultanacar11@gmail.com>
2025-02-12 17:11:38 +0300

    Counting upper and lower cases and determining the equality task

--- src/upper_case_lower_case/UpperCaseLowerCase.java
+++ src/upper_case_lower_case/UpperCaseLowerCase.java
@@ -4,12 +4,20 @@ public class UpperCaseLowerCase {
 
     public static void main(String[] args) {
 
-        String str = "JAVA java";
+        countingUpperLowerCases("JAVA java");
+        countingUpperLowerCases("programming LANGUAGE");
+
+    }
+
+    /**
+     * A void method for counting upper and lower cases and determining the equality
+     * @param str
+     */
+    public static void countingUpperLowerCases(String str){
         int totalUpperCase = 0;
         int totalLowerCase = 0;
 
         for (char ch: str.toCharArray()){
-
             if (Character.isUpperCase(ch)){
                 totalUpperCase++;
             }else if (Character.isLowerCase(ch)){
@@ -18,10 +26,14 @@ public class UpperCaseLowerCase {
         }
 
         if (totalUpperCase==totalLowerCase){
-            System.out.println(true);
+            System.out.println("Total UpperCase is equal to total LowerCase: " + true);
         } else{
-            System.out.println(false);
+            System.out.println("Total UpperCase is equal to total LowerCase: " + false);
         }
 
     }
+
+
+
+
 }
