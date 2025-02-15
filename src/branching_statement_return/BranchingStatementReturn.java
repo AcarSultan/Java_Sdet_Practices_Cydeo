9db075fd7769a69dd9910d5acf6948700e384da5
AcarSultan <sultanacar11@gmail.com>
2025-02-15 19:02:24 +0300

    continue, break and return branching statements

--- src/branching_statement_return/BranchingStatementReturn.java
+++ src/branching_statement_return/BranchingStatementReturn.java
@@ -5,9 +5,11 @@ public class BranchingStatementReturn {
 
         for (int i = 1; i < 6; i++) {
             if (i==3){
+
                 // continue; // skips // jumps to the next iteration of the loop
                 // break; // terminates or exits the loop // jumps out of the loop's body
                 // return; // terminates the method pr exits the method // jumps out of the method
+
             }
             System.out.println(i);
         }
