57e29660ad5b260ad1b40ca4d3037eb7697ccdf1
AcarSultan <sultanacar11@gmail.com>
2025-02-16 23:46:31 +0300

    choosing a language

--- src/choose_language/ChooseLanguage.java
+++ src/choose_language/ChooseLanguage.java
@@ -3,20 +3,36 @@ package choose_language;
 public class ChooseLanguage {
     public static void main(String[] args) {
 
-        int selection = 3;
-
-        if (selection==1){
-            System.out.println("Hello, thank you for your call");
-        } else if (selection==2) {
-            System.out.println("Hola, gracias por llamar");
-        } else if (selection==3) {
-            System.out.println("Merhaba, aradığınız için teşekkürler");
-        } else if (selection==4) {
-            System.out.println("Привет, спасибо за ваш звонок");
-        } else if (selection==5){
-            System.out.println("Merci, pour votre appel");
-        }
+        System.out.println(choosingLanguage(1));
 
 
     }
+
+    /**
+     * Custom method for choosing a language
+     * @param selection int
+     * @return result
+     */
+    public static String choosingLanguage(int selection){
+
+        String result= null;
+
+        if (selection==1){
+            result = "Hello, thank you for your call";
+        } else if (selection==2) {
+            result = "Hola, gracias por llamar";
+        } else if (selection==3) {
+            result = "Merhaba, aradığınız için teşekkürler";
+        } else if (selection==4) {
+            result = "Привет, спасибо за ваш звонок";
+        } else if (selection==5){
+            result = "Merci, pour votre appel";
+        }
+
+        return result;
+
+    }
+
+
+
 }
