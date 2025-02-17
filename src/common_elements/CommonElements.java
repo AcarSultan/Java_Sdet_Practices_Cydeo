a7aca1715e4db607266d8934785a3f8b1027abcd
AcarSultan <sultanacar11@gmail.com>
2025-02-17 17:45:10 +0300

    Finding common elements between two treesets

--- src/common_elements/CommonElements.java
+++ src/common_elements/CommonElements.java
@@ -8,15 +8,32 @@ public class CommonElements {
 
     public static void main(String[] args) {
 
-        Set<Integer> set1 = new TreeSet<>(Arrays.asList(10, 8, 11, 20, 5, 0));
-        Set<Integer> set2 = new TreeSet<>(Arrays.asList(30, 0, 20, 40, 50, 10, 60));
-        Set<Integer> set3 = new TreeSet<>();
-        for (Integer each : set1) {
+        TreeSet<Integer> set1 = new TreeSet<>(Arrays.asList(10, 8, 11, 20, 5, 0));
+        TreeSet<Integer> set2 = new TreeSet<>(Arrays.asList(30, 0, 20, 40, 50, 10, 60));
+
+        System.out.println(findingCommonElements(set1, set2));
+
+    }
+
+
+    /**
+     * Custom method for finding common elements between 2 TreeSets
+     * @param set1 TreeSet
+     * @param set2 TreeSet
+     * @return set3
+     */
+    public static TreeSet findingCommonElements(TreeSet set1, TreeSet set2){
+
+        TreeSet<Integer> set3 = new TreeSet<>();
+
+        for (Object each : set1) {
             if (set2.contains(each)){
-                set3.add(each);
+                set3.add((Integer) each);
             }
         }
-        System.out.println(set3);
+        return set3;
+
     }
+
 
 }
