9fc15a207f6d36ccec00e1e129fb31381e323906
AcarSultan <sultanacar11@gmail.com>
2025-03-07 22:10:40 +0300

    Finding the duplicates and then remove it

--- src/remove_duplicates/RemoveDuplicates.java
+++ src/remove_duplicates/RemoveDuplicates.java
@@ -7,18 +7,29 @@ import java.util.Collections;
 public class RemoveDuplicates {
     public static void main(String[] args) {
 
-        ArrayList<Integer> list= new ArrayList<>(Arrays.asList(1,1,2,2,3,3,4,4,5,6,7));
+        ArrayList list = removeDuplicates(new ArrayList<>(Arrays.asList(1,1,2,2,3,3,4,4,5,6,7)));
+        System.out.println("list = " + list);
+
+    }
+
+
+    /**
+     * An ArrayList method for finding the duplicates and then remove it
+     * @param list ArrayList<Integer>
+     * @return list
+     */
+    public static ArrayList removeDuplicates(ArrayList<Integer> list){
 
         for (int i=0; i<list.size(); i++){
             if (Collections.frequency(list,i)>1){
                 list.remove(i);
             }
         }
-        System.out.println(list);
-
-
-
-
-
+        return list;
     }
+
+
+
+
+
 }
